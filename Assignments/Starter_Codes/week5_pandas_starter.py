f06b0d76c55126e4fc7ff6c259bf21cd4ddd7b05
Laura Condon <lecondon@email.arizona.edu>
2020-10-08 07:33:51 -0700

    adding rubric image

--- Assignments/Starter_Codes/week5_pandas_starter.py
+++ Assignments/Starter_Codes/week5_pandas_starter.py
@@ -18,17 +18,17 @@ print(filepath)
 filepath = '../Assignments/Solutions/data/streamflow_week1.txt'
 
 # %%
-#Read the data into a pandas dataframe
-data=pd.read_table(filepath, sep = '\t', skiprows=30,
-        names=['agency_cd', 'site_no', 'datetime', 'flow', 'code']
+# Read the data into a pandas dataframe
+data = pd.read_table(filepath, sep = '\t', skiprows=30,
+        names =['agency_cd', 'site_no', 'datetime', 'flow', 'code']
         )
 
 # Expand the dates to year month day
-data[["year", "month", "day"]] =data["datetime"].str.split("-", expand=True)
+data[["year", "month", "day"]] = data["datetime"].str.split("-", expand=True)
 data['year'] = data['year'].astype(int)
 data['month'] = data['month'].astype(int)
 data['day'] = data['day'].astype(int)
 
 # %%
-# Sorry no more helpers past here this week, you are on your own now :) 
+# Sorry no more helpers past here this week, you are on your own now :)
 # Hints - you will need the functions: describe, info, groupby, sort, head and tail.
